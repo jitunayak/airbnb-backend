1cf5db5c6a680cd0058a2f0a545cf57c90d10171
jitunayak <jitunayak715@gmail.com>
2023-08-19 21:53:00 +0530

    trying to fix cors policy

--- main.ts
+++ main.ts
@@ -4,10 +4,9 @@ import wishlistRouter from "./src/wishlist_router.ts";
 const PORT = Deno.env.get("PORT") as unknown as number || 8000;
 const app = new Application();
 
+app.use(oakCors({ origin: "*" }));
 app.use(wishlistRouter.routes());
 app.use(wishlistRouter.allowedMethods());
-
-app.use(oakCors({ origin: "*" }));
 
 console.log(`Server is running on port ${PORT} 🔥`);
 await app.listen({ port: PORT });
