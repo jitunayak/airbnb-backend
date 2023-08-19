c52091a2beb179e567bfcbb367fa0b2b625a2c07
jitunayak <jitunayak715@gmail.com>
2023-08-19 22:54:07 +0530

    fixes delete all api

--- src/wishlist_router.ts
+++ src/wishlist_router.ts
@@ -1,6 +1,7 @@
 import { Router } from "https://deno.land/x/oak@v12.5.0/router.ts";
 import {
   addToWishList,
+  deleteAllWishListItems,
   getAllWishLists,
   removeFromWishList,
 } from "./wishlist_dao.ts";
@@ -18,12 +19,14 @@ router.get("/:userId", async (ctx) => {
 
 router.post("/:userId", async (ctx) => {
   const body = await ctx.request.body().value;
-  await addToWishList(ctx.params.userId, JSON.parse(body));
+  await addToWishList(ctx.params.userId, body);
   ctx.response.body = body;
 });
+
 router.delete("/:userId/all", async (ctx) => {
-    
-})
+  await deleteAllWishListItems(ctx.params.userId);
+  ctx.response.body = JSON.stringify("Deleted all items from wish list");
+});
 
 router.delete("/:userId", async (ctx) => {
   const body = await ctx.request.body().value;
