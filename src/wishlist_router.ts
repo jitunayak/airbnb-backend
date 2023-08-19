726ae96541037a65bb4b362fbe1d42667eec9865
jitunayak <jitunayak715@gmail.com>
2023-08-19 20:53:37 +0530

    bug fixes

--- src/wishlist_router.ts
+++ src/wishlist_router.ts
@@ -1,17 +1,14 @@
 import { Router } from "https://deno.land/x/oak@v12.5.0/router.ts";
-
-import { WishlistDao } from "./wishlist_dao.ts";
+import { addToWishList, getAllWishLists } from "./wishlist_dao.ts";
 
 const router = new Router({ prefix: "/api/v1/wishlists" });
-const kv = await Deno.openKv();
-const wishlistDao = new WishlistDao(kv);
 
 router.get("/:userId", async (ctx) => {
   if (!ctx.params.userId) {
     ctx.response.status = 404;
     ctx.response.body = "User Id is missing";
   }
-  const result = await wishlistDao.getAllWishLists(ctx.params.userId);
+  const result = await getAllWishLists(ctx.params.userId);
   ctx.response.body = result;
 });
 
@@ -27,7 +24,7 @@ router.get("/:userId", async (ctx) => {
 
 router.post("/:userId", async (ctx) => {
   const body = await ctx.request.body().value;
-  await wishlistDao.addToWishList(ctx.params.userId, body);
+  await addToWishList(ctx.params.userId, body);
   ctx.response.body = body;
 });
 
